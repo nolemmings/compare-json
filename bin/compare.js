4fa1efc963c52307c4494bc5a136ec084d67c9e6
Niels Krijger <niels@kryger.nl>
2015-10-19 10:20:18 +0200

    Switch to yargs to improve help message

--- bin/compare.js
+++ bin/compare.js
@@ -1,8 +1,15 @@
 #!/usr/bin/env node
 
-var argv = require('minimist')(process.argv.slice(2));
-var cli = require('../lib/cli');
+var argv = require('yargs')
+  .usage('Usage: comparejson -e ./test/*.json')
+  .option('exit', {
+    alias: 'e',
+    describe: 'exit with error code when object keys were are missing',
+    type: 'boolean'
+  })
+  .help('help')
+  .argv;
 
-console.log(argv);
+var cli = require('../lib/cli');
 
 cli(argv._, argv);
